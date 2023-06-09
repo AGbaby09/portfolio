a70cb6aa2cdf07bdfac73b5f16d795bfde56583b
Agbotse Selasi Michael <agbotsem@gmail.com>
2023-06-09 19:18:42 +0000

    commit

--- src/components/forDesktop/port/Portfolio.js
+++ src/components/forDesktop/port/Portfolio.js
@@ -1,10 +1,28 @@
 import './Portfolio.css';
+import Photo from './portIMG.jpg';
 const Portfolio = () => {
     return ( 
         <main id='port'>
-            <h1><u>Portfolio</u></h1>
+            <i className='bx bxl-unity' ></i>
+            <i className='bx bxl-meta'></i>
+            <i className='bx bx-loader-circle' ></i>
+            <i className='bx bx-circle-three-quarter' ></i>
+            <i className='bx bx-shape-triangle'></i>
+            <div id='portName'>
+                <h1>HI, I'M</h1>
+                <h1>AGBOTSE SELASI MICHAEL</h1>
+            </div>
+            <div id='portSummary'>
+                <button>Personal Blog</button>
+                <button>Tech Blog</button>
+                <button>Skill Set</button>
+                <button>Projects</button>
+            </div>
+            <div id='portPhoto'>
+                <img src={Photo} alt=''/>
+            </div>
         </main>
-     );
+    );
 }
  
 export default Portfolio;
