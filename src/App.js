48b8df8cf6e79ddf0100e1b8e4529b5bd78fc0eb
Agbotse Selasi Michael <agbotsem@gmail.com>
2023-06-22 23:27:01 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
-import About from './components/forDesktop/company/Company';
-import Portfolio from './components/forDesktop/port/Portfolio';
+import About from './components/forDesktop/company/Company.js';
+import Portfolio from './components/forDesktop/port/Portfolio.js';
 import Landing from 'C:/Users/HP/Desktop/react/portfolio-project/portfolio/src/components/forDesktop/about/Landing.js';
 import Thanks from 'C:/Users/HP/Desktop/react/portfolio-project/portfolio/src/components/forDesktop/thanks/Thanks.js';
-import Contact from 'C:/Users/HP/Desktop/react/portfolio-project/portfolio/src/components/forDesktop/contact/Contact.js';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <About />
       <Portfolio />
       <Thanks />
-      <Contact />
     </div>
   );
 }
--- src/components/forDesktop/thanks/Thanks.js
+++ src/components/forDesktop/thanks/Thanks.js
@@ -2,7 +2,39 @@ import './Thanks.css';
 const Thanks = () => {
     return ( 
         <section id='thanks'>
-            <h1>All Glory and Honor to Almighty God</h1>
+            <h1>SKILLS</h1>
+            <ul>
+                <h3>Programming Languages</h3>
+                <li>Python</li>
+                <li>Java</li>
+                <li>C++</li>
+                <li>PHP</li>
+                <li>Javascript</li>
+                <li>Dart</li>
+
+            </ul>
+            <ul>
+                <h3>Programming Skills</h3>
+                <li>Problem Solving and Algorithm Design</li>
+                <li>Data Structures and Algorithm</li>
+                <li>Object Oriented Programming Principles</li>
+                <li>Debugging and Troubleshooting</li>
+            </ul>
+            <ul>
+                <h3>Web App Development</h3>
+                <li>HTML</li>
+                <li>Vanilla CSS & JS</li>
+                <li>React JS</li>
+                <li>Next JS</li>
+                <li>Node JS</li>
+            </ul>
+            
+            <ul>
+                <h3>Mobile App & Database Management</h3>
+                <li>Flutter</li>
+                <li>MySQL</li>
+                <li>Mongo DB</li>
+            </ul>
         </section>
      );
 }
